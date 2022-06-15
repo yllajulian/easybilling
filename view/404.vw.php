0fb35db096d681844b9bba547f61a5b01623a0c6
yllajulian <yllajulian@gmail.com>
2022-06-15 16:05:08 -0500

    primera modificacion

--- view/404.vw.php
+++ view/404.vw.php
@@ -1,6 +1,6 @@
 <div class="overlay"></div>
 	<div class="terminal">
-		  <h1>Error Ruta no encontrada <span class="errorcode">404</span></h1>
+		  <h1>Error<span class="errorcode">404</span></h1>
 		  <p class="output">La página que está buscando podría haber sido eliminada, ha
 			cambiado de Nombre  o no está disponible temporalmente</p>
 		   <p class="output">Por favor regrese al <a href="./?View=Inicio"> Inicio </a></p>
